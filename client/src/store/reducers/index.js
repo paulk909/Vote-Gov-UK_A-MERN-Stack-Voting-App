9652217edde135e17d557ed4c485bb90f6d8fc62
Paul Kerr <pkerr205@caledonian.ac.uk>
2020-04-26 01:33:02 +0100

    First commit

---
+++ client/src/store/reducers/index.js
@@ -0,0 +1,12 @@
+import {combineReducers} from 'redux';
+
+import auth from './auth';
+import error from './error';
+import {polls, currentPoll} from './polls';
+
+export default combineReducers({
+    auth,
+    error,
+    polls,
+    currentPoll
+});
